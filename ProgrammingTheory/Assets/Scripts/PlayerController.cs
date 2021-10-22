d8aec967a1f7cb72b3d6ab85409327da820ce08a
dgab27 <damgabrys@gmail.com>
2021-10-22 22:49:05 +0200

    implemented player movement
    - rigidbody movement by pressing horizontal input
    - added boxes to contstrain the player to viewport

--- ProgrammingTheory/Assets/Scripts/PlayerController.cs
+++ ProgrammingTheory/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRB;
+    private float speed = 2000;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      float horizontalInput = Input.GetAxis("Horizontal");
+      playerRB.AddForce(Vector3.right * speed * horizontalInput * Time.deltaTime);
+
     }
+
 }
