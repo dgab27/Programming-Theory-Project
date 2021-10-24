b316513708f778a22a6ab0bf9a2362f65f1af2f1
dgab27 <damgabrys@gmail.com>
2021-10-24 02:54:36 +0200

    Added Shape
    - added shape base class
    - first steps into encapsulation (setting properties)
    - shape destroys on collision and prints a message to the game log

--- ProgrammingTheory/Assets/Scripts/Shape.cs
+++ ProgrammingTheory/Assets/Scripts/Shape.cs
@@ -1,9 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Shape : MonoBehaviour
 {
+    private string shapeName = "Shape";
+
+    public string Name
+    {
+        get
+        {
+            return shapeName;
+        }
+        set
+        {
+            name = value;
+        }
+    }
+
+    int scoreToGive = 5;
+
+    public int ScoreToGive
+    {
+        get
+        {
+            return scoreToGive;
+        }
+        set
+        {
+            scoreToGive = value;
+        }
+    }
+    public TextMeshProUGUI textToDisplay;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +46,23 @@ public class Shape : MonoBehaviour
     {
         
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            DisplayText();
+        }
+        else if (collision.gameObject.tag == "Ground")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void DisplayText()
+    {
+        textToDisplay.text = "Player caught: " + shapeName + " and received " + scoreToGive + " score";
+    }
+
 }
